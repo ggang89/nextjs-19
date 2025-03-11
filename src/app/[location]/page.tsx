3344490beda445c4f5f424f6c7f1add5cdb920fa
ggang89 <ggang89@gmail.com>
2025-03-11 20:14:31 +0900

    날씨 API 키 받아서 그려주고 env파일 설정

--- src/app/[location]/page.tsx
+++ src/app/[location]/page.tsx
@@ -5,11 +5,14 @@ type Props = {
     location: string;
   };
 };
+
 // params: 루트 세그먼트에서 해당 페이지로 내려가는 동적 경로 매개 변수가 포함된 프로미스
 function Detail({ params }: Props) {
+ 
   return (
     <>
       <h1 className="text-3xl">{params.location}의 날씨</h1>
+    
       <HomeButton />
     </>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import Link from "next/link";
 
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY 
+
+const getCurrentWeather = async () => {
+  const res = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London&aqi=no`
+  )
+
+  if (!res.ok) {
+    if (!res.ok) throw new Error('날씨 정보를 가져올 수 없습니다.')
+  }
+  return res.json()
+};
+
 export default function Home() {
+   const res =  getCurrentWeather()
+  console.log(res)
   return (
     <>
       <h1 className="text-4xl font-bold">Home</h1>
