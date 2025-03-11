ad97de56161b89a8f3075b73d25f84e9d58c3060
ggang89 <ggang89@gmail.com>
2025-03-11 18:48:16 +0900

    페이지간 이동하기(Link이용하거나 useRouter 이용하기)

--- src/app/[location]/page.tsx
+++ src/app/[location]/page.tsx
@@ -1,15 +1,24 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+
 type Props = {
   params: {
     location: string;
-  };  
-}
+  };
+};
 // params: 루트 세그먼트에서 해당 페이지로 내려가는 동적 경로 매개 변수가 포함된 프로미스
-function Detail({params}:Props) {
+function Detail({ params }: Props) {
+  const router = useRouter();
+  const handleClick = () => { 
+    router.push('/');
+  };
   return (
     <>
-      <h1>Detail {params.location }</h1>
+      <h1>{params.location}의 날씨</h1>
+     <button onClick={handleClick}>go back Home</button>
     </>
-  )
+  );
 }
 
 export default Detail;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div >
-      Home
-    </div>
+    <>
+      <h1>Home</h1>
+      <h2>날씨</h2>
+      <ul>
+        <li>
+          <Link href="/seoul">서울</Link>
+        </li>
+        <li>
+          {" "}
+          <Link href="/newyork">뉴욕</Link>
+        </li>
+      </ul>
+    </>
   );
 }
